ab911bc822953fb288c70459458897fa3aab5c68
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2025-03-09 08:09:26 -0600

    Add psotgresql connection and cursor.

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ Download stock data from Yahoo Finance, transform data in SQL, and load data int
 
 from pathlib import Path
 from stock_data_pipeline.load_yfinance_data import CollectDailyData
+import psycopg2
+
+HOST = "localhost"
+PORT = 5432
+DATABASE = "stock_history"
+USER = "postgres"
+PASSWORD = "l"
+
+DB_PARAMS = {
+    "host": HOST,
+    "port": PORT,
+    "dbname": DATABASE,
+    "user": USER,
+    "password": PASSWORD,
+}
 
 # Create list of stocks
 stocks = []
@@ -13,6 +28,9 @@ with open(stocks_file_path, "r", encoding="utf-8") as file:
     for stock in file:
         stocks.append(stock.rstrip("\n"))
 
+def connect_postgresql_local_server():
+    connection = psycopg2.connect(**DB_PARAMS)
+    return connection, connection.cursor()
 
 stock_data_directory = Path("data")
 for stock in stocks:
