f7965d927cbc5bb94b9d69a0d667b14b4c901543
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2025-04-26 08:05:58 -0600

    Add headless mode.

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ DOWNLOAD_DIR.mkdir(exist_ok=True)
 options = webdriver.ChromeOptions()
 prefs = {"download.default_directory": download_file_directory_absolute_path}
 options.add_experimental_option("prefs", prefs)
-# options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
