7d9861cf741626539bd47d88e858db7eb8ef1816
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2025-05-06 06:05:56 -0600

    Remove import of selenium By since cell_type is passed into press_button.

--- stock_data_pipeline/chrome_driver.py
+++ stock_data_pipeline/chrome_driver.py
@@ -3,7 +3,6 @@ import time
 from pathlib import Path
 from shutil import rmtree
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
